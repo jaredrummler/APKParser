b3b836b1ee669e43631ba887cb7e773351ef0c06
Jared Rummler <jared@jaredrummler.com>
2015-11-13 15:42:07 -0800

    Github prettify theme

--- sample/src/main/java/com/jaredrummler/apkparser/sample/activities/AndroidManifestActivity.java
+++ sample/src/main/java/com/jaredrummler/apkparser/sample/activities/AndroidManifestActivity.java
@@ -110,7 +110,11 @@ public class AndroidManifestActivity extends AppCompatActivity {
 
   private void loadSourceCode(String html) {
     String data = String.format(
-        "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /><script src=\"run_prettify.js?skin=sons-of-obsidian\"></script></head><body bgcolor=\"#000000\"><pre class=\"prettyprint linenums\">%s</pre></body></html>",
+        "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3" +
+            ".org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"><html xmlns=\"http://www.w3" +
+            ".org/1999/xhtml\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; " +
+            "charset=utf-8\" /><script src=\"run_prettify.js?skin=github\"></script></head><body " +
+            "bgcolor=\"white\"><pre class=\"prettyprint linenums\">%s</pre></body></html>",
         html);
     webView.loadDataWithBaseURL("file:///android_asset/", data, "text/html", "UTF-8", null);
   }
